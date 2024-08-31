712986f20d59cba57a7abc35f2f1f5361b5e584e
Luong <luongvqph33534@fpt.edu.vn>
2024-08-31 20:37:47 +0700

    save

--- BE/src/app.js
+++ BE/src/app.js
@@ -3,7 +3,6 @@ import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
 
-
 import cors from "cors";
 import dotenv from "dotenv";
 import morgan from "morgan";
@@ -22,7 +21,7 @@ const app = express();
 dotenv.config();
 //Middleware
 app.use(express.json());
-app.use(morgan("tiny"));
+// app.use(morgan("tiny"));
 app.use(cors());
 //connect db
 connectDB(process.env.DB_URI);
@@ -32,12 +31,12 @@ app.use("/api", productRouter);
 app.use("/api", attributeRouter);
 app.use("/api", attributeValueRouter);
 app.use("/api", routerCategory);
-app.use("/api", routerCart)
+app.use("/api", routerCart);
 app.use("/api", routerVoucher);
-app.post("/api/products/add", addProduct)
-app.post("/api/variant/add", createVariant)
-app.delete("/api/variant/:id", removeVariant)
-app.post("/api/user/add", createUser)
+app.post("/api/products/add", addProduct);
+app.post("/api/variant/add", createVariant);
+app.delete("/api/variant/:id", removeVariant);
+app.post("/api/user/add", createUser);
 // app.use("/api", authRouter);
 
 export const viteNodeApp = app;
