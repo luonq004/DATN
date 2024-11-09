e9da24d3dc257f5721bf976e78d7f82e3c12c93a
Luong <luongvq004@gmail.com>
2024-11-10 00:05:25 +0700

    fix

--- FE/src/main.tsx
+++ FE/src/main.tsx
@@ -2,11 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
 import "./index.css";
-import "swiper/css";
+// import "swiper.css";
 import "react-toastify/dist/ReactToastify.css";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router-dom";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+// import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { ClerkProvider } from "@clerk/clerk-react";
 
 const queryClient = new QueryClient({
@@ -29,7 +29,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
       <BrowserRouter>
         <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
           <App />
-          <ReactQueryDevtools initialIsOpen={false} />
+          {/* <ReactQueryDevtools initialIsOpen={false} /> */}
         </ClerkProvider>
       </BrowserRouter>
     </QueryClientProvider>
