e22115141929e72aa61ef7a013d6491224be5706
HoangMinh456 <xinphepgiauten456@gmail.com>
2024-11-29 19:12:16 +0700

    data admin side-bar

--- FE/src/pages/(dashboard)/layout.tsx
+++ FE/src/pages/(dashboard)/layout.tsx
@@ -38,7 +38,7 @@ const LayoutAdmin = () => {
     return null;
   }
 
-  console.log("user", user);
+  // console.log("user", user);
 
   // Nếu đã xác thực quyền truy cập, render giao diện
   return (
