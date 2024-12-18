dfcce609b832c01843d6d0ad172ec09e5291cae5
Dokhanh15 <dokhanh526@gmail.com>
2024-12-18 17:23:02 +0700

    fix

--- FE/src/pages/(website)/user/page.tsx
+++ FE/src/pages/(website)/user/page.tsx
@@ -1,6 +1,10 @@
+import React from "react";
 import ProfilePageModern from "./_components/ProfileUser";
 
 const ProfilePage = () => {
+  React.useEffect(() => {
+        document.title = "Thông Tin Cá Nhân"; 
+      }, []);
   return (
     <div className="flex">
       <ProfilePageModern />
